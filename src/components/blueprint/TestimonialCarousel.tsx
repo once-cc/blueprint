6331aaa132b8192c384587146ea4dd954926f4e3
Joshua Tipene Jon Cleland <kingjoshua@Joshuas-MacBook-Air.local>
2026-02-25 17:03:06 +1300

    style: remove background from testimonial carousel

--- src/components/blueprint/TestimonialCarousel.tsx
+++ src/components/blueprint/TestimonialCarousel.tsx
@@ -17,11 +17,7 @@ export function TestimonialCarousel() {
   const animatedX = useSpring(x, { stiffness: 400, damping: 40 });
 
   return (
-    <div className="relative w-full overflow-hidden py-24 bg-background select-none">
-
-      {/* Background Gradient Fades to mask edges smoothly */}
-      <div className="absolute left-0 top-0 bottom-0 w-24 md:w-64 bg-gradient-to-r from-background to-transparent z-20 pointer-events-none" />
-      <div className="absolute right-0 top-0 bottom-0 w-24 md:w-64 bg-gradient-to-l from-background to-transparent z-20 pointer-events-none" />
+    <div className="relative w-full overflow-hidden py-24 bg-transparent select-none">
 
       {/* 3D Scene Container */}
       <div
